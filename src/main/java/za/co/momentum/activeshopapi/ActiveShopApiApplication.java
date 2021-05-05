9c6636d7dd5709222482669e2ba97146e8178a4e
dasusingtest <dasindurban@gmail.com>
2021-05-05 20:53:17 +0200

    Initial commit

---
+++ src/main/java/za/co/momentum/activeshopapi/ActiveShopApiApplication.java
@@ -0,0 +1,13 @@
+package za.co.momentum.activeshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/momentum/activeshopapi/ActiveShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.momentum.activeshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
